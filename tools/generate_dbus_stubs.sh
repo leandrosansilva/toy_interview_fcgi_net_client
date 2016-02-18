10a664793149a4df809543a4d7442a99c6133df0
Leandro Santiago <leandrosansilva@gmail.com>
2016-02-18 22:39:32 +0100

    Ops, added tool to generate dbus stubs

---
+++ tools/generate_dbus_stubs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+SOURCE_DIR=$1
+BINARY_DIR=$2
+XML_DIR=$3
+PREFIX=$4
+
+STUBS_DIR=$BINARY_DIR/dbus_stubs/
+
+mkdir -p $STUBS_DIR/$PREFIX
+
+XML2CPP=$BINARY_DIR/3rd/bin/dbusxx-xml2cpp
+TPL_FILE=$SOURCE_DIR/3rd/dbus-cplusplus/tools/proxy-stubs.tpl
+
+for XML_FILE in $XML_DIR/*.xml; do
+  BASENAME=$(basename $XML_FILE)
+  H_FILE=$STUBS_DIR/$PREFIX/${BASENAME}.h
+  $XML2CPP $XML_FILE --proxy=$H_FILE --proxy-template=$SOURCE_DIR/3rd/dbus-cplusplus/tools/proxy-stubs.tpl
+done
